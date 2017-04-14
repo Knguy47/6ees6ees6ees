3845a5da91b3f13cdce3e1b267f2ed58e4054c48
Kevin Nguyen <knguy47@gmail.com>
2017-04-14 13:45:35 -0500

    Added Solutions

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,23 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee{
+  constructor(){
+    super();
+    this.age = 40;
+    this.job = 'gamble';
+    this.honeyPot = 0;
+    this.color = 'grey';
+    this.canFly = false;
+    this.treasureChest = [];
+  }
   // TODO..
+  forage(){
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(){
+    var treasure = true;
+    if(treasure){
+      this.treasureChest.push(treasure);
+    }
+  }
+
 };
